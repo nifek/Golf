46ae45cf8af4713c1e6f6e0bf8e91bbcd03b50b2
arseni <r.seny.martysiuk@gmail.com>
2025-12-09 18:26:19 +0100

    fix: fix firebase auth config

--- GolfApp/GolfApp.swift
+++ GolfApp/GolfApp.swift
@@ -1,7 +1,12 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct GolfApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     @StateObject private var appState = AppState()
 
     var body: some Scene {
